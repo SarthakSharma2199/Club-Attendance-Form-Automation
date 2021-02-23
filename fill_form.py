99a386d346f5dd5885b9be0d1fc4e08a06323a79
Sarthak Sharma <sarthaksharma2199@outlook.com>
2021-02-24 01:15:53 +0530

    automated dropdown clicks

--- fill_form.py
+++ fill_form.py
@@ -7,7 +7,7 @@ print(participants)
 
 web=webdriver.Chrome("./chromedriver.exe")
 
-web.get('https://docs.google.com/forms/d/e/1FAIpQLSckKFDYqXQ9MeN-1YkfwiHEUgswCnpwCggOixFDX38cUxrCSA/viewform')
+web.get('https://docs.google.com/forms/d/e/1FAIpQLSfJ9cOpTOMpJhm_3pia04ocgGCEE70RyEEoYjOSgEtMt5O9qg/viewform')
 # web.implicitly_wait(5)
 time.sleep(1)
 
@@ -17,3 +17,24 @@ club_or_chapter_name="Dance Club"
 
 form_email=web.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div[1]/div[2]/div[1]/div/div[1]/input')
 form_email.send_keys(club_email)
+
+club_or_chapter_dropdown=web.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div[1]/div[1]')
+club_or_chapter_dropdown.click()
+time.sleep(1)
+club_or_chapter_option = web.find_elements_by_xpath("//div//span[contains(., 'Club')]")
+for i in club_or_chapter_option:
+    try:
+        i.click()
+    except Exception as e:
+        print(e)
+
+club_or_chapter_name_dropdown=web.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div[1]/div[1]')
+club_or_chapter_name_dropdown.click()
+time.sleep(1)
+club_or_chapter_name_option = web.find_elements_by_xpath("//div//span[contains(., 'Dance club')]")
+for i in club_or_chapter_name_option:
+    try:
+        i.click()
+    except Exception as e:
+        print(e)
+
