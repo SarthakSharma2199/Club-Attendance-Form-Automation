960403708f7ee42e0990d14cd277615f74964057
Sarthak Sharma <sarthaksharma2199@outlook.com>
2021-02-24 13:56:42 +0530

    basic working script ready

---
+++ variables.py
@@ -0,0 +1,11 @@
+# Add your club email
+club_email="demo@gmail.com"
+#Choose option as Club/Chapter (remember to capitalise)
+club_or_chapter="Club"
+#Enter the club or chapter name exactly as it appears in the form
+club_or_chapter_name="Dance Club"
+#Enter your event name
+event_name="Testing"
+#Enter the event date in dd-mm-yyyy format
+#For example 23-02-2021 must be entered as 23022021
+event_date="23022021"
